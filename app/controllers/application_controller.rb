0cd00b444e22cd087c834523be7e7bdcfb732207
Haloren <mckennakyler@gmail.com>
2020-06-19 16:17:23 -0700

    ApplicationControler configure set up, sessions secret, and layout with  main.css fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 
 class ApplicationController < Sinatra::Base
 
+    configure do 
+        set :public_folder, "public"
+        set :views, "app/views"
+        enable :sessions
+        set :session_secret, ENV['SESSION_SECRET']
+    end
+
     get '/' do
-        "Login"
+        erb :'users/login'
     end
 
 end

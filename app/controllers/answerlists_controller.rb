8d1af660e4aa2f2e2ec5d1541f9fb18c5a9e1599
Haloren <mckennakyler@gmail.com>
2020-06-21 12:31:04 -0700

    added authenticate and @user to answerlists for validations and layout function

--- app/controllers/answerlists_controller.rb
+++ app/controllers/answerlists_controller.rb
@@ -1,26 +1,50 @@
 class AnswerListsController < ApplicationController
 
     get "/answerlists" do
+        authenticate
+        @user = current_account
+        # binding.pry
 
+        
     end
 
     post "/answerlists" do
+        authenticate
+        @user = current_account
+        # binding.pry
+
 
     end
 
     get "/answerlists/new" do
+        authenticate
+        @user = current_account
+        # binding.pry
+
 
     end
 
     get "/answerlists/:id" do
+        authenticate
+        @user = current_account
+        # binding.pry
+
 
     end
 
     get "/answerlists/:id/update" do
+        authenticate
+        @user = current_account
+        # binding.pry
+
 
     end
 
     patch "/answerlists/:id" do
+        authenticate
+        @user = current_account
+        # binding.pry
+
 
     end
 
@@ -31,7 +55,7 @@ class AnswerListsController < ApplicationController
         
         @list = AnswerList.find_by(id: params[:id])
         @list.destroy
-        
+
         redirect to '/answerlists'
     end
 

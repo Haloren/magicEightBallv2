dd88d650d8bf73d0cd6e6aab26ddc2ab99a83c30
Haloren <mckennakyler@gmail.com>
2020-06-21 12:20:25 -0700

    set up answerlist route structure

--- app/controllers/answerlists_controller.rb
+++ app/controllers/answerlists_controller.rb
@@ -1,6 +1,31 @@
 class AnswerListsController < ApplicationController
 
+    get "/answerlists" do
 
+    end
 
+    post "/answerlists" do
+
+    end
+
+    get "/answerlists/new" do
+
+    end
+
+    get "/answerlists/:id" do
+
+    end
+
+    get "/answerlists/:id/update" do
+
+    end
+
+    patch "/answerlists/:id" do
+
+    end
+
+    delete "/answerlists/:id/delete" do
+        
+    end
 
 end

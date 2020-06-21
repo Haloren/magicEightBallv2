faba4e0d6ad7db518836b13df314ae549a59ec2a
Haloren <mckennakyler@gmail.com>
2020-06-20 23:05:14 -0700

    created helpers to verify user and provided a default answer list for the eight ball

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,43 @@ class ApplicationController < Sinatra::Base
         erb :'users/login'
     end
 
-    
+    helpers do 
+        def current_account
+            # binding.pry
+            @current_account ||= User.find_by_id(session[:user_id])
+        end
+       
+        def logged_in?
+            !!current_account
+        end
+
+        def authenticate
+            redirect '/login' if !logged_in?
+        end
+
+        def default_answers
+            @default = ["As I see it, yes.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don’t count on it.",
+                "It is certain.",
+                "It is decidedly so.",
+                "Most likely.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Outlook good.",
+                "Reply hazy, try again.",
+                "Signs point to yes.",
+                "Very doubtful.",
+                "Without a doubt.",
+                "Yes.",
+                "Yes – definitely.",
+                "You may rely on it."].sample
+        end
+
+    end
 
 end
